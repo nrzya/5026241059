8934ee997621390ac215e02dc41c581448e14311
Nayla <naylarameyzaa@gmail.com>
2026-04-30 22:48:54 +0700

    pertemuan 7

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+ <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <u>www.malasngoding.com</u>";
+});
+
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('pert5', function () {
+	return view('pertemuan5');
+});
+
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
+Route::get('index', function () {
+	return view('index');
+});
+
+Route::get('belajar', function () {
+	return view('belajar');
+});
+
+Route::get('intro', function () {
+	return view('intro');
+});
+
+Route::get('layout4', function () {
+	return view('layout4');
+});
+
+Route::get('news', function () {
+	return view('news');
+});
+
+Route::get('responsive', function () {
+	return view('responsive');
+});
+
+
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
+
