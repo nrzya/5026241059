5b64fc8212f45376e51af44af027376bdf5851fd
Nayla <naylarameyzaa@gmail.com>
2026-06-10 12:44:19 +0700

    LATIHAN EAS

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -30,14 +30,14 @@
                         <a class="nav-link" href="#">Siswa</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Latihan 1</a>
+                        <a class="nav-link" href="/nilaikuliah">Latihan 1</a>
                     </li>
 
                     <li class="nav-item">
                         <a class="nav-link" href="#">Latihan 2</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Latihan EAS</a>
+                        <a class="nav-link" href="#">EAS</a>
                     </li>
                 </ul>
             </div>
