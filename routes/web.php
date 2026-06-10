80a7ed5e6763de8913ff5cf619fde9cbb7fe07f7
Nayla <naylarameyzaa@gmail.com>
2026-06-10 08:04:27 +0700

    pertemuan 14

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\Pegawaidbcontroller;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -77,3 +78,11 @@ Route::get('/pegawai/edit/{id}', [PegawaiDBcontroller::class, 'edit']);
 Route::post('/pegawai/update', [PegawaiDBcontroller::class, 'update']);
 Route::get('/pegawai/hapus/{id}', [PegawaiDBcontroller::class, 'hapus']);
 Route::get('/pegawai/cari', [PegawaiDBcontroller::class, 'cari']);
+
+//route CRUD siswa
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+Route::get('/siswa/{nrp}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::put('/siswa/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
